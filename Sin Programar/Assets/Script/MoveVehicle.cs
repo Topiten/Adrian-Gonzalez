23f765b209e09371af6a4848f9f132c56037dff7
Topiten <62898835+Topiten@users.noreply.github.com>
2020-04-12 20:29:56 -0300

    Movimiento del vehiculo
    Cree el script de movimiento del vehiculo y agregue un vehiculo a la escena

---
+++ Sin Programar/Assets/Script/MoveVehicle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveVehicle : MonoBehaviour
+{
+    public float speed;
+    public float speedReverse;
+    public float speedRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speedReverse * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+        {
+            transform.RotateAround (transform.position, Vector3.down, speedRotate * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+        {
+            transform.RotateAround(transform.position, Vector3.up, speedRotate * Time.deltaTime);
+        }
+
+    }
+}
