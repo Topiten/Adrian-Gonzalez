31384f198499942920708ca5ac93f7389fa29aab
Topiten <62898835+Topiten@users.noreply.github.com>
2020-04-12 23:24:07 -0300

    Cree el script tank
    Cree el script tank, ahora el tanque puede disparar y tiene balas

---
+++ Sin Programar/Assets/Script/Tank.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MoveVehicle 
+{
+    public int bullets;
+    public GameObject prefabBullets;
+    public float distanceBullets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bullets >= 0 && Input.GetKey (KeyCode.F))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(prefabBullets,new Vector3(transform.position.x, transform.position.y, transform.position.z + distanceBullets), transform.rotation);
+    }
+}
