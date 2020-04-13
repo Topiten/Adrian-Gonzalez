26dcc81baa3e77b356f64e8ba6e6611e8659359b
Topiten <62898835+Topiten@users.noreply.github.com>
2020-04-12 21:19:41 -0300

    Cree el Script FollowObject
    Cree un script para que la camara siga al vehiculo

---
+++ Sin Programar/Assets/Script/FollowObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform followObject;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, followObject.position.z + distance);
+    }
+}
